71e975d0e30062ce3bf514affbecbee819f24579
Parker Shumard <parkersam@gmail.com>
2024-12-12 01:12:38 -0700

    Fixed file paths

--- src/files/ConvertWordFile.ts
+++ src/files/ConvertWordFile.ts
@@ -1,8 +1,8 @@
 import { readFileSync, writeFileSync } from "fs";
 
 function convertFile() {
-  const inFile = "./word-lists/combined_wordlist.txt";
-  const outFile = "./word-lists/all_words.txt";
+  const inFile = "./word-lists/raw-lists/combined_wordlist.txt";
+  const outFile = "./word-lists/PossibleGuesses.txt";
 
   const data = readFileSync(inFile, { encoding: "utf-8" });
   const list = data.split("\n").sort();
